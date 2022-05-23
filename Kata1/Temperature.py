74de01e977ad5acb87d2889d6424b4930ef32826
Belkis Vaq <55562967+BelkisVasquez0609@users.noreply.github.com>
2022-05-22 21:22:43 -0400

    Methods toFarenheit, toKelvin, toCelsius and add

---
+++ Kata1/Temperature.py
@@ -0,0 +1,51 @@
+def __toFahrenheit__(grade, scale):
+    if scale == "C":
+        result = (grade * 1.8) + 32
+    elif scale == "K":
+        result = 1.8 * (grade - 273) + 32
+    else:
+        result = grade
+    return result
+
+
+def __toCelsius__(grade, scale):
+    if scale == "F":
+        result = (grade - 32) * 0.555555559
+    elif scale == "K":
+        result = (grade - 273.15)
+    else:
+        result = grade
+    return result
+
+
+def __toKelvin__(grade, scale):
+    if scale == "C":
+        result = (grade - 273.15)
+    elif scale == "F":
+        result = 5 / 9(grade - 32) + 273.15
+    else:
+        result = grade
+    return result
+
+
+class Temperature:
+
+    def __init__(self, Grade, Scale):
+        self.grade = Grade
+        self.scale = Scale
+
+    def __add__(self, other):
+        if isinstance(other, Temperature):
+            if self.scale == "F":
+                result = __toFahrenheit__(other.grade, other.scale)
+            elif self.scale == "C":
+                result = __toCelsius__(other.grade, other.scale)
+            elif self.scale == "K":
+                result = __toKelvin__(other.grade, other.scale)
+            else:
+                result = 0
+                self.scale = "No valid"
+
+            return Temperature(self.grade + result, self.scale)
+        else:
+            return "No Valid"
---
+++ Kata1/TemperatureScalar.py
@@ -0,0 +1,4 @@
+class TemperatureScalar:
+    Kelvin = "K"
+    Fahrenheit = "F"
+    Celsius = "C"
