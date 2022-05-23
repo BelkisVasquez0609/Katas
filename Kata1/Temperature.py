4d236878fbbebeb28515979da96f15289321f5d7
Belkis Vaq <55562967+BelkisVasquez0609@users.noreply.github.com>
2022-05-22 21:44:23 -0400

    fix kelvin result on toFahrenheit Method

--- Kata1/Temperature.py
+++ Kata1/Temperature.py
@@ -2,7 +2,7 @@ def __toFahrenheit__(grade, scale):
     if scale == "C":
         result = (grade * 1.8) + 32
     elif scale == "K":
-        result = 1.8 * (grade - 273) + 32
+        result = 1.8 * (grade - 273.15) + 32
     else:
         result = grade
     return result
@@ -46,6 +46,54 @@ class Temperature:
                 result = 0
                 self.scale = "No valid"
 
-            return Temperature(self.grade + result, self.scale)
+            return Temperature(round(self.grade + result, 2), self.scale)
+        else:
+            return "No Valid"
+
+    def __substractBy__(self, other):
+        if isinstance(other, Temperature):
+            if self.scale == "F":
+                result = __toFahrenheit__(other.grade, other.scale)
+            elif self.scale == "C":
+                result = __toCelsius__(other.grade, other.scale)
+            elif self.scale == "K":
+                result = __toKelvin__(other.grade, other.scale)
+            else:
+                result = 0
+                self.scale = "No valid"
+
+            return Temperature(round(self.grade - result, 2), self.scale)
+        else:
+            return "No Valid"
+
+    def __divideBy__(self, other):
+        if isinstance(other, Temperature):
+            if self.scale == "F":
+                result = __toFahrenheit__(other.grade, other.scale)
+            elif self.scale == "C":
+                result = __toCelsius__(other.grade, other.scale)
+            elif self.scale == "K":
+                result = __toKelvin__(other.grade, other.scale)
+            else:
+                result = 0
+                self.scale = "No valid"
+
+            return Temperature(round(self.grade / result, 2), self.scale)
+        else:
+            return "No Valid"
+
+    def __multiplicateBy__(self, other):
+        if isinstance(other, Temperature):
+            if self.scale == "F":
+                result = __toFahrenheit__(other.grade, other.scale)
+            elif self.scale == "C":
+                result = __toCelsius__(other.grade, other.scale)
+            elif self.scale == "K":
+                result = __toKelvin__(other.grade, other.scale)
+            else:
+                result = 0
+                self.scale = "No valid"
+
+            return Temperature(round(self.grade * result, 2), self.scale)
         else:
             return "No Valid"
--- Kata1/main.py
+++ Kata1/main.py
@@ -1,4 +1,11 @@
+from Temperature import Temperature
+from TemperatureScalar import TemperatureScalar
 
 if __name__ == '__main__':
-    print("Hello")
+     v1 = Temperature(38.4, TemperatureScalar.Fahrenheit)
+     v2 = Temperature(93, TemperatureScalar.Kelvin)
+
+     v3 = v1.__substractBy__(v2)
+     print(f"(Grade: {v3.grade}, Scalar: {v3.scale})")
+
 
